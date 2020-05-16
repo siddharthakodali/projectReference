698077f236185a8d4416e0df72ab2c5b1320a8f9
C841431 <siddhartha.kodali@bnsf.com>
2020-05-16 09:31:12 -0500

    Intial Project set up

---
+++ src/test/java/com/projectreference/reference/ReferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectreference.reference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReferenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
